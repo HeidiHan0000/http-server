3588c245a5faf2d32a3ba0128d0540a69010ba08
unknown <hanheidibms@gmail.com>
2024-05-10 13:54:42 -0400

    respond 200

--- app/main.py
+++ app/main.py
@@ -1,16 +1,40 @@
-# Uncomment this to pass the first stage
 import socket
 
+def handle_request(client_socket):
+    received_msg = client_socket.recv(1024)
+    if not received_msg:
+        return False
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.send(response.encode())
+    print("Sent 200")
+    return True
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    # reuse_port not avail on Windows
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
+    print("Server running on port 4221...")
+    # added timeout because CTLR-C will not work
+    server_socket.settimeout(1.0)
+    
+    try:
+        client_socket = None
+        while True:
+            try:
+                client_socket, client_addr = server_socket.accept()
+                handle_request(client_socket)
+            except socket.timeout:
+                pass
+            except IOError as msg:
+                print(msg)
+                server_socket.close()
+                print("Server shut down")
+                break
+            finally:
+                if client_socket:
+                    client_socket.close()
+    except KeyboardInterrupt:
+        server_socket.close()
+        print("Server shut down")
 
 if __name__ == "__main__":
     main()
